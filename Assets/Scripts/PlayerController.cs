2dd50c4da6fa08b0e479a95f57bc72ccd3f3d525
Pata309 <patricijakuze@gmail.com>
2021-04-15 11:53:24 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class PlayerController : MonoBehaviour
+{
+    public float totalLife = 30.0f;
+    public Slider healthBar;
+    // Use this for initialization
+    void Start()
+    {
+        // Određivanje maksimalnog broja na slider elementu
+        healthBar.maxValue = totalLife;
+        // Određivanje trenutnog broja na slider elementu
+        healthBar.value = totalLife;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // Provjera da li je preostalo života
+        if (totalLife > 0)
+        {
+            // Ako je, preostali život se smanjuje
+            totalLife = totalLife - 10.0f;
+            // Određivanje trenutnog broja na slider elementu
+            healthBar.value = totalLife;
+        }
+    }
+}
